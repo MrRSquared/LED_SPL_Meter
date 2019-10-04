6d9ac0e738abef853c0710920741963297e1848b
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2019-10-04 06:38:33 -0400

    Update and rename 3 to LEDSoun3.ino

--- LEDSoun3.ino
+++ LEDSoun3.ino
@@ -4,11 +4,11 @@
 #define MIC_PIN   15  // Microphone is attached to Trinket GPIO #2/Gemma D2 (A1)
 #define DATA_PIN    21  // NeoPixel LED strand is connected to GPIO #0 / D0
 #define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
-#define NOISE     0  // Noise/hum/interference in mic signal
+#define NOISE     00  // Noise/hum/interference in mic signal
 #define SAMPLES   60  // Length of buffer for dynamic level adjustment
 #define TOP       (NUM_LEDS -1) // Allow dot to go slightly off scale
-#define INPUT_FLOOR 10 //Lower range of analogRead input
-#define INPUT_CEILING 50 //Max range of analogRead input, the lower the value the more sensitive (1023 = max)
+#define INPUT_FLOOR 0 //Lower range of analogRead input
+#define INPUT_CEILING 300 //Max range of analogRead input, the lower the value the more sensitive (1023 = max)
 
 CRGB leds[NUM_LEDS];
 
@@ -33,7 +33,7 @@ void setup() {
   delay(100);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   // brightness = 75;
-  FastLED.setBrightness(15);
+  FastLED.setBrightness(10);
   for(q=0; q<NUM_LEDS; q++) {  
        leds[q].setRGB(0,0,0);
     } 
@@ -47,6 +47,7 @@ void loop() {
   uint8_t  i;
   uint16_t minLvl, maxLvl;
   int      n, height;
+  int oldPeak = 0;
   
   
   n   = analogRead(MIC_PIN);    // Raw reading from mic 
@@ -57,13 +58,13 @@ void loop() {
 
   height = TOP * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
   //height = TOP * lvl;
-  height = map(height, 1800, 0, INPUT_CEILING, INPUT_FLOOR);
+  //height = map(height, 1800, 25, INPUT_CEILING, INPUT_FLOOR);
   
 
   if(height < 0L)       height = 0;      // Clip output
   else if(height > TOP) height = TOP;
-  if (count < 1) peak = height;
-  if (peak < height)     peak   = height; // Keep 'peak' dot at top
+  if (count < 1) oldPeak = height;
+  if (peak < height )     peak   = height; // Keep 'peak' dot at top
 
 
   vol[volCount] = n;                      // Save sample for dynamic leveling
@@ -73,20 +74,22 @@ void loop() {
 
   //FastLED.show();
   for(i=0; i<NUM_LEDS; i++) { 
-    if(i == peak)               
-       leds[i].setRGB(50,   0, 0); 
-    else if (i < height)
-      leds[i].setRGB(50,0,50); 
-    else if (i>= height && i != peak)
+    //if(i == peak)               
+    //   leds[i].setRGB(50,   0, 0); 
+    if (i < height)
+     leds[i].setRGB(50,0,50); 
+    else if (i>= height) //&& i != peak)
       leds[i].setRGB(0,0,0); 
     } 
 
    FastLED.show(); // Update strip
 
    // Draw peak dot  
-  //if(peak > 0 && peak <= NUM_LEDS-1) leds[peak].setRGB(100,0,0);
+  if(peak > 0 && peak <= NUM_LEDS-1 && peak >= oldPeak) leds[peak].setRGB(60,0,0);
+      oldPeak = peak;
+
   
-   //FastLED.show(); // Update strip
+   FastLED.show(); // Update strip
 
   // Get volume range of prior frames
   minLvl = maxLvl = vol[0];
@@ -104,69 +107,4 @@ void loop() {
    minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
    maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
   count ++;
-}
-
-float fscale( float originalMin, float originalMax, float newBegin, float
-newEnd, float inputValue, float curve){
- 
-  float OriginalRange = 0;
-  float NewRange = 0;
-  float zeroRefCurVal = 0;
-  float normalizedCurVal = 0;
-  float rangedValue = 0;
-  boolean invFlag = 0;
- 
- 
-  // condition curve parameter
-  // limit range
- 
-  if (curve > 10) curve = 10;
-  if (curve < -10) curve = -10;
- 
-  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
-  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
- 
-  /*
-   Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
-   Serial.println(); 
-   */
- 
-  // Check for out of range inputValues
-  if (inputValue < originalMin) {
-    inputValue = originalMin;
-  }
-  if (inputValue > originalMax) {
-    inputValue = originalMax;
-  }
- 
-  // Zero Refference the values
-  OriginalRange = originalMax - originalMin;
- 
-  if (newEnd > newBegin){ 
-    NewRange = newEnd - newBegin;
-  }
-  else
-  {
-    NewRange = newBegin - newEnd; 
-    invFlag = 1;
-  }
- 
-  zeroRefCurVal = inputValue - originalMin;
-  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
- 
-  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine 
-  if (originalMin > originalMax ) {
-    return 0;
-  }
- 
-  if (invFlag == 0){
-    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
- 
-  }
-  else     // invert the ranges
-  {   
-    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange); 
-  }
- 
-  return rangedValue;
 }
